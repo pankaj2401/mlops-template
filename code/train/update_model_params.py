df03bf412f9c0d18c36b46c6387439ff4891a801
SreekarSNP <106574670+SreekarSNP@users.noreply.github.com>
2022-06-27 13:42:30 +0530

    Update and rename sample.py to update_model_params.py

---
+++ code/train/update_model_params.py
@@ -0,0 +1,23 @@
+import sys
+import json 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+
+# Getting existing model parameters
+register_model_file = open("code/train/registermodel.json", "r")
+model_json_object = json.load(register_model_file)
+register_model_file.close()
+
+# Add run id to the tags
+model_json_object["tags"]["run_id"] = str(sys.argv[1])
+register_model_file = open("code/train/registermodel.json", "w")
+json.dump(model_json_object, register_model_file)
+register_model_file.close()
